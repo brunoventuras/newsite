f0d5c37d2e0738f345db3164320cbe04d1d05571
Bruno Ventura <brunoventuras@gmail.com>
2020-12-08 12:00:54 -0300

    form teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-  return <div>Hello World</div>
+  return <div>
+      <form id="form3" method="POST">
+        <input type="text" id="texto" />
+        <input type="submit" id="btnS" />
+      </form>
+    </div>
 }
 
 export default Home
